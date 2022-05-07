fa43f481280475c6a5bb5e2a521be56078f517a4
Dmitry Kisler <admin@dkisler.com>
2022-05-07 16:36:22 +0200

    add cs and assembler codebase

---
+++ cs/main.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Factorial
+{
+    class Main
+    {
+        // static int i;
+
+        // // factorial calculates factorial given not negative int input.
+        // static int factorial(int n) => n < 2 ? 1 : n * factorial(n - 1);
+
+        // // readStdin reads user's input from stdin as int.
+        // static void readStdin()
+        // {
+        //     BufferedReader br = new BufferedReader(new InputStreamReader(Console.In));
+        //     try
+        //     {
+        //         i = Integer.parseInt(br.readLine());
+        //     }
+        //     catch (IOException e)
+        //     {
+        //         Console.WriteLine(e);
+        //     }
+        // }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
